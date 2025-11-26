6109c14e3fe21f2b581f5d6c9703301891449977
和輝 <kazukiqianchuan85@gmail.com>
2025-11-26 18:57:53 +0900

    ユーザー検索のプロフィール詳細画面のCSS完了

--- resources/views/authenticated/users/profile.blade.php
+++ resources/views/authenticated/users/profile.blade.php
@@ -3,35 +3,60 @@
   <div class="top_area w-75 m-auto pt-5">
     <span>{{ $user->over_name }}</span><span>{{ $user->under_name }}さんのプロフィール</span>
     <div class="user_status p-3">
+
       <p>名前 : <span>{{ $user->over_name }}</span><span class="ml-1">{{ $user->under_name }}</span></p>
       <p>カナ : <span>{{ $user->over_name_kana }}</span><span class="ml-1">{{ $user->under_name_kana }}</span></p>
       <p>性別 : @if($user->sex == 1)<span>男</span>@else<span>女</span>@endif</p>
       <p>生年月日 : <span>{{ $user->birth_day }}</span></p>
-      <div>選択科目 :
+
+      <div class="mb-2">選択科目 :
         @foreach($user->subjects as $subject)
-        <span>{{ $subject->subject }}</span>
+          <span>{{ $subject->subject }}</span>
         @endforeach
       </div>
-      <div class="">
-        @can('admin')
-        <span class="subject_edit_btn">選択科目の編集</span>
+
+      @can('admin')
+      <div>
+        <span class="subject_edit_btn">選択科目の編集 ▼</span>
+
         <div class="subject_inner">
           <form action="{{ route('user.edit') }}" method="post">
-            @foreach($subject_lists as $subject_list)
-            <div>
-              <label>{{ $subject_list->subject }}</label>
-              <input type="checkbox" name="subjects[]" value="{{ $subject_list->id }}">
+
+            <div class="subject-items">
+              @foreach($subject_lists as $subject_list)
+              <label class="subject-item">
+                <input type="checkbox" name="subjects[]" value="{{ $subject_list->id }}">
+                {{ $subject_list->subject }}
+              </label>
+              @endforeach
+
+              <button type="submit" class="btn btn-primary ml-3">登録</button>
             </div>
-            @endforeach
-            <input type="submit" value="編集" class="btn btn-primary">
+
             <input type="hidden" name="user_id" value="{{ $user->id }}">
             {{ csrf_field() }}
           </form>
         </div>
-        @endcan
       </div>
+      @endcan
+
     </div>
   </div>
 </div>
+
+{{-- ▼ 開閉スクリプト --}}
+<script>
+  $(".subject_edit_btn").on("click", function () {
+      $(this).toggleClass("open");
+
+      if ($(this).hasClass("open")) {
+        $(this).text("選択科目の編集 ▲");
+      } else {
+        $(this).text("選択科目の編集 ▼");
+      }
+
+      $(".subject_inner").slideToggle();
+  });
+</script>
 
 </x-sidebar>
